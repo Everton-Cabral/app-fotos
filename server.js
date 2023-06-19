e7702e667dd4109fa5772cd2aae956a90d6dded4
Everton Cabral <90525060+Everton-Cabral@users.noreply.github.com>
2023-06-19 13:16:33 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const multer = require('multer');
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+
+// Rota para upload de imagens
+app.post('/upload', upload.array('images'), (req, res) => {
+  // Processar as imagens recebidas aqui
+  console.log(req.files); // Exemplo: exibir as informações dos arquivos recebidos
+
+  // Retornar uma resposta adequada para o cliente
+  res.send('Imagens recebidas com sucesso!');
+});
+
+// Iniciar o servidor na porta definida ou na porta 3000
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
